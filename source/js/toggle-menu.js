9e5fbca509d0a0bf815e2e964844993d929438fc
Anatoly230 <65662008+Anatoly230@users.noreply.github.com>
2022-05-12 21:12:05 +0300

    меню работает без  js каталог предварительно завершен

--- source/js/toggle-menu.js
+++ source/js/toggle-menu.js
@@ -1,6 +1,11 @@
 const toggleMenu = document.querySelector(".togle-menu");
 const mainMenu = document.querySelector(".menu");
 
+if (!mainMenu.classList.contains("menu--hide")) {
+  console.log("yes");
+  mainMenu.classList.add("menu--hide");
+}
+
 toggleMenu.addEventListener("click", function (e) {
   e.preventDefault;
   if (e.target.classList.contains("togle-menu")) {
