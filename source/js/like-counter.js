36d9cd5f90cf8f256f884bc94e2d44e578cbfc8c
Anatoly230 <65662008+Anatoly230@users.noreply.github.com>
2022-05-13 22:18:10 +0300

    откорректирована вёрстка к 'Декоративные элементы главной страницы'

---
+++ source/js/like-counter.js
@@ -0,0 +1,2 @@
+const likeBox = document.querySelectorAll(".card__like-wrapper");
+const likeBtn = document.querySelectorAll(".card__like-btn");
--- source/js/toggle-menu.js
+++ source/js/toggle-menu.js
@@ -1,13 +1,15 @@
 const toggleMenu = document.querySelector(".togle-menu");
 const mainMenu = document.querySelector(".menu");
 
+
 if (!mainMenu.classList.contains("menu--hide")) {
-  console.log("yes");
   mainMenu.classList.add("menu--hide");
+} else {
+  toggleMenu.classList.add("togle-menu--hide");
 }
 
 toggleMenu.addEventListener("click", function (e) {
-  e.preventDefault;
+  e.preventDefault();
   if (e.target.classList.contains("togle-menu")) {
     mainMenu.classList.toggle("menu--hide");
     toggleMenu.classList.toggle("togle-menu--close");
