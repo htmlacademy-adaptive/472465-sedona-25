40b234eb8b4d0020b79498a2aac012d98dc16efb
Keks <keks@htmlacademy.ru>
2022-05-19 23:13:28 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/toggle-menu.js
@@ -0,0 +1,17 @@
+const toggleMenu = document.querySelector(".togle-menu");
+const mainMenu = document.querySelector(".menu");
+
+
+if (!mainMenu.classList.contains("menu--hide")) {
+  mainMenu.classList.add("menu--hide");
+} else {
+  toggleMenu.classList.add("togle-menu--hide");
+}
+
+toggleMenu.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("togle-menu")) {
+    mainMenu.classList.toggle("menu--hide");
+    toggleMenu.classList.toggle("togle-menu--close");
+  }
+})
